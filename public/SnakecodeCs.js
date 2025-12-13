959e5e0566591e98d35020bfea9d3f36f5c9007f
Quaza638 <samuelpre0218@gmail.com>
2025-12-13 23:20:50 +0800

    commit

--- public/SnakecodeCs.js
+++ public/SnakecodeCs.js
@@ -7,16 +7,67 @@ var board;
 var context;
 
 
+//snake
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+var velocityX = 0;
+var velocityY = 0;
+
+//food 
+var foodX;
+var foodY;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
     context = board.getContext("2d");
 
-    update();
+    placeFood();
+    document.addEventListener("keyup", changeDirection)
+    //update();
+    this.setInterval(update, 1000/10); //100 ms
+    
 }
 
 function update() {
     context.fillStyle="black";
     context.fillRect(0,0,board.width,board.height);
+    
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+    
+    if (snakeX == foodX && snakeY == foodY) {
+        placeFood();
+    }
+
+    context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+}
+
+function changeDirection(e) {
+    if ((e.code == "ArrowUp" && velocityY != 1) || (e.code == "KeyW" && velocityY != 1) ) {
+        velocityX = 0;
+        velocityY = -1; 
+    }
+    else if ((e.code == "ArrowDown" && velocityY != -1) || (e.code == "KeyS" && velocityY != -1)) {
+        velocityX = 0;
+        velocityY = 1; 
+    }
+    else if ((e.code == "ArrowLeft" && velocityX != 1) || (e.code == "KeyA" && velocityX != 1)) {
+        velocityX = -1;
+        velocityY = 0; 
+    }
+    else if ((e.code == "ArrowRight" && velocityX != -1) || (e.code == "KeyD" && velocityX != -1)) {
+        velocityX = 1;
+        velocityY = 0 ;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
